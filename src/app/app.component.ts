03b8a98bdf5e59561106240db7663ea828d61d5c
Samip Jain <samipjain1216@gmail.com>
2020-08-18 22:17:45 -0400

    Add list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'autocomplete';
+
+  queryString: string;
+
+  dataset = ['samip', 'helena', 'abhishek', 'prafull', 'samipi'];
+
+  searchResults: Array<String> = this.dataset;
+
+  onSubmit() {
+    this.fetch(this.queryString).then((res) => { 
+      this.searchResults = res;
+    }).catch(err => console.log(err));
+  }
+
+  fetch(queryParam: string) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.dataset.filter(d => d === queryParam ))
+      }, 1000);
+    })
+  }
 }
